18397a050f58ef5b86aaf4f6c1ce1c69cedf57ad
Opraveen <pparihar522@gmail.com>
2024-05-03 14:16:02 +0530

    Add files via upload

---
+++ test_trans.py
@@ -0,0 +1,18 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, upper
+
+# Initialize Spark session
+spark = SparkSession.builder \
+    .appName("CSV to Uppercase") \
+    .getOrCreate()
+
+# Read CSV file into DataFrame
+df = spark.read.option("header", True).csv("/home/opraveen/airflow_workspace/airflow/netflix_titles.csv")
+
+# Apply transformation: Convert "description" column to uppercase
+df_transformed = df.withColumn("description", upper(col("description")))
+
+# Write transformed DataFrame to output location
+df_transformed.write.mode("overwrite").csv("/home/opraveen/airflow_workspace/airflow/op_output.csv")
+
+spark.stop()
